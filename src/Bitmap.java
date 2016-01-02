20bc8501f63eb118a92da79ad0898a87d89801ca
Gareth <gareth.w.owen@gmail.com>
2016-01-02 15:33:08 +0000

    We don't need no bit shifting!

--- src/Bitmap.java
+++ src/Bitmap.java
@@ -7,7 +7,7 @@ public class Bitmap
   // Width in height of image
   private final int m_height;
   // Every pixel component of image
-  private final char m_components[];
+  private final byte m_components[];
 
 
   public Bitmap(int width, int height)
@@ -23,16 +23,17 @@ public class Bitmap
     * be a multiple of 4. Again, weird, but doing a[x][y] = blah has proven annoying in
     * the past...
     */
-    m_components = new char[width * height * 4];
+    m_components = new byte[width * height * 4];
   }
 
   // clears the bitmap to a shade of gray
-  public void Clear(char shade)
+  public void Clear(byte shade)
   {
     Arrays.fill(m_components, shade);
   }
 
-  public void DrawPixel(int x, int y, char a, char r, char g, char b)
+  // sets the pixel at (x, y) to the colour specified as (a, b, g, r)
+  public void DrawPixel(int x, int y, byte a, byte b, byte g, byte r)
   {
     // represents the index of the cpt we need to start modifiying
     int index = (x + y + m_width) * 4;
@@ -42,22 +43,16 @@ public class Bitmap
     m_components[index + 3] = b;
   }
 
-  public void CopyToIntArray(int[] dest)
+  public void CopyToByteArray(byte[] dest)
   {
     for(int i = 0; i < m_width * m_height; i++)
     {
-      /* chars = 1 byte in Java
-      *  int = 4 bytes in Java
-      *  So we do some bit manipulation to get them correctly aligned.
-      *  8 bits in a byte, so we do... 8 for g, 16 for b and so on...
-      */
-      int a =  ((int)m_components[i * 4]) << 24;
-      int r =  ((int)m_components[i * 4 + 1]) << 16;
-      int g =  ((int)m_components[i * 4 + 2]) << 8;
-      int b =  ((int)m_components[i * 4 + 3]);
-
-      // We do a bitwise OR on our components to form a single integer
-      dest[i] = a | r | g | b;
+      // so basically, we can use the byte type in Java instead, so
+      // we're going to discard the alpha component for now. So now it's
+      // i * 3 (destination) = i * 4 + 1 to skip over the alpha component
+      dest[i * 3] = m_components[i* 4 + 1];
+      dest[i * 3 + 1] = m_components[i* 4 + 2];
+      dest[i * 3 + 2] = m_components[i* 4 + 3];
     }
   }
 }
