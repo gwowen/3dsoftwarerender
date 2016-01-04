30150651e16e0b030ff4eb3fe7c9cf8ebce832fe
Gareth <gareth.w.owen@gmail.com>
2016-01-04 23:35:22 +0000

    Initial setup of RenderContext funcs

---
+++ src/RenderContext.java
@@ -0,0 +1,19 @@
+public class RenderContext extends Bitmap
+{
+  private final int m_scanBuffer[];
+
+  public RenderContext(int width, int height)
+  {
+
+  }
+
+  public void DrawScanBuffer(int yCoord, int xMin, int xMax)
+  {
+
+  }
+
+  public void FillShape(int yMin, int yMax)
+  {
+
+  }
+}
