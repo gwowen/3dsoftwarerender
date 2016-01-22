8b808f57e63beabfd1ac5de85b5ad0985027b637
Gareth <gareth.w.owen@gmail.com>
2016-01-22 00:08:27 +0000

    Began work on Mat4f class

--- src/Mat4f.java
+++ src/Mat4f.java
@@ -4,27 +4,50 @@ public class Mat4f
 
   public Mat4f()
   {
-
+    m = new float[4][4]
   }
 
   public Mat4f InitIdentity()
   {
+    m[0][0] = 1; m[0][1] = 0; m[0][2] = 0; m[0][3] = 0;
+    m[1][0] = 0; m[1][1] = 1; m[1][2] = 0; m[1][3] = 0;
+    m[2][0] = 0; m[2][1] = 0; m[2][2] = 1; m[2][3] = 0;
+    m[3][0] = 0; m[3][1] = 0; m[3][2] = 0; m[3][3] = 1;
 
+    return this;
   }
 
   public Mat4f InitScreenSpaceTransform(float halfWidth, float halfHeight)
   {
+    m[0][0] = halfWidth; m[0][1] = 0; m[0][2] = 0; m[0][3] = halfWidth;
+    m[1][0] = 0; m[1][1] =-halfHeight; m[1][2] = 0; m[0][2] = halfHeight;
+    m[2][0] = 0; m[2][1] = 0; m[2][2] = 1; m[2][3] = 0;
+    m[3][0] = 0; m[3][1] = 0; m[3][2] = 0; m[3][3] = 1;
 
+    return this;
   }
 
   public Mat4f InitTranslation(float x, float y, float z)
   {
+    m[0][0] = 1; m[0][1] = 0; m[0][2] = 0; m[0][3] = x;
+    m[1][0] = 0; m[1][1] = 1; m[1][2] = 0; m[0][2] = y;
+    m[2][0] = 0; m[2][1] = 0; m[2][2] = 1; m[2][3] = z;
+    m[3][0] = 0; m[3][1] = 0; m[3][2] = 0; m[3][3] = 1;
 
+    return this;
   }
 
   public Mat4f InitRotation(float x, float y, float z, float angle)
   {
+    float sin = (float)Math.sin(angle);
+    float cos = (float)Math.cos(angle);
 
+    m[0][0] = cos + x*x*(1-cos); m[0][1] = x*y*(1-cos)-z*sin; m[0][2] = x*z*(1-cos) + y*sin; m[0][3] = 0;
+    m[1][0] = y*x*(1-cos) + z*sin; m[1][1] = cos + y*y*(1-cos); m[1][2] = y*z*(1-cos) - x*sin; m[1][3] = 0;
+    m[2][0] = z*x*(1-cos) - y*sin; m[2][1] = z*y*(1-cos) + x*sin; m[2][2] = cos + z*z*(1-cos); m[2][3] = 0;
+    m[3][0] = 0; m[3][1] = 0; m[3][2] = 0; m[3][3] = 1;
+
+    return this;
   }
 
   public Mat4f InitRotation(float x, float y, float z)
