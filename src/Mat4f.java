4cbc77f49bd897ca08e07e29832be9f7ef4d6a4f
Gareth <gareth.w.owen@gmail.com>
2016-01-27 00:34:23 +0000

    Finished off maths clases

--- src/Mat4f.java
+++ src/Mat4f.java
@@ -52,42 +52,130 @@ public class Mat4f
 
   public Mat4f InitRotation(float x, float y, float z)
   {
+    Mat4f rx = new Mat4f();
+    Mat4f ry = new Mat4f();
+    Mat4f rz = new Mat4f();
+
+    rz.m[0][0] = (float)Math.coz(z); rz.m[0][1] = -(float)Math.sin(z); rz.m[0][2] = 0; rz.m[0][3] = 0;
+    rz.m[1][0] = (float)Math.sin(z); rz.m[1][1] = (float)Math.cos(z); rz.m[1][2] = 0; rz.m[1][3] = 0;
+    rz.m[2][0] = 0; rz.m[2][1] = 0; rz.m[2][2] = 1; rz.m[2][3] = 0;
+    rz.m[3][0] = 0; rz.m[3][1] = 0; rz.m[3][2] = 0; rz.m[3][3] = 1;
+
+    rx.m[0][0] = 1; rx.m[0][1] = 0; rx.m[0][2] = 0; rx.m[0][3] = 0;
+    rx.m[1][0] = 0; rx.m[1][1] = (float)Math.cos(x); rx.m[1][2] = -(float)Math.sin(x); rx.m[1][3] = 0;
+    rx.m[2][0] = 0; rx.m[2][1] = (float)Math.sin(x); rx.m[2][2] = (float)Math.cos(x); rx.m[2][3] = 0;
+    rx.m[3][0] = 0; rx.m[3][1] = 0; rx.m[3][2] = 0; rx.m[3][3] = 1;
+
+    ry.m[0][0] = (float)Math.cos(y); ry.m[0][1] = 0; ry.m[0][2] = -(float)Math.sin(x); ry.m[0][3] = 0;
+    ry.m[1][0] = 0; ry.m[1][1] = 1; ry.m[1][2] = 0; ry.m[1][3] = 0;
+    ry.m[2][0] = (float)Math.sin(y); ry.m[2][1] = 0; ry.m[2][2] = (float)Math.cos(y); ry.m[2][3] = 0;
+    ry.m[3][0] = 0; ry.m[3][1] = 0; ry.m[3][2] = 0; ry.m[3][3] = 1;
+
+    m = rz.Mul(ry.Mul(rx)).GetM();
+    return this;
 
   }
 
   public Mat4f InitScale(float x, float y, float z)
   {
+    m[0][0] = x; m[0][1] = 0; m[0][2] = 0; m[0][3] = 0;
+    m[1][0] = 0; m[1][1] = y; m[1][2] = 0; m[1][3] = 0;
+    m[2][0] = 0; m[2][1] = 0; m[2][2] = z; m[2][3] = 0;
+    m[3][0] = 0; m[3][1] = 0; m[3][2] = 0; m[3][3] = 1;
 
+    return this;
   }
 
   public Mat4f InitPerspective(float fov, float aspectRatio, float zNear, float zFar)
   {
+    float tanHalfFOV = (float)Math.tan(fov / 2);
+    float zRange = zNear - zFar;
 
+    m[0][0] = 1.0f / (tanHalfFOV * aspectRatio); m[0][1] = 0; m[0][2] = 0; m[0][3] = 0;
+    m[1][0] = 0; m[1][1] = 1.0f / tanHalfFOV; m[1][2] = 0; m[1][3] = 0;
+    m[2][0] = 0; m[2][1] = 0; m[2][2] = (-zNear -zFar) / zRange; m[2][3] = 0;
+    m[3][0] = 0; m[3][1] = 0; m[3][2] = 1; m[3][3] = 0;
+
+    return this;
   }
 
   public Mat4f InitOrthographic(float left, float right, float bottom, float top, float near, float far)
   {
+    float width = right - left;
+    float height = top - bottom;
+    float depth = far - near;
 
+    m[0][0] = 2 / width; m[0][1] = 0; m[0][2] = 0; m[0][3] = -(right + left) / width;
+    m[1][0] = 0; m[1][1] = 2 / height; m[1][2] = 0; m[1][3] = -(top + bottom) / height;
+    m[2][0] = 0; m[2][1] = 0; m[2][2] = - 2 / depth; m[2][3] = -(far + near) / depth;
+    m[3][0] = 0; m[3][1] = 0; m[3][2] = 0; m[3][3] = 1;
+
+    return this;
   }
 
   public Mat4f InitRotation(Vec4f forward, Vec4f up)
   {
+    Vec4f f  = forward.Normalized();
 
+    Vec4f r = up.Normalized();
+
+    r = r.Cross(f);
+
+    Vec4f u = f.Cross(r);
+
+    return InitRotation(f, u, r);
+  }
+
+  public Mat4f InitRotation(Vec4f forward, Vec4f up, Vec4f right)
+  {
+    Vec4f f = forward;
+    Vec4f r = right;
+    Vec4f u = up;
+
+    m[0][0] = r.GetX(); m[0][1] = r.GetY(); m[0][2] = r.GetZ(); m[0][3] = 0;
+    m[1][0] = u.GetX(); m[1][1] = u.GetY(); m[1][2] = u.GetZ(); m[1][3] = 0;
+    m[2][0] = f.GetX(); m[2][1] = f.GetY(); m[2][2] = f.GetZ(); m[2][3] = 0;
+    m[3][0] = 0; m[3][1] = 0; m[3][2] = 0; m[3][3] = 1;
+
+    return this;
   }
 
   public Vec4f Transform(Vec4f r)
   {
+    return new Vec4f(m[0][0] * r.GetX() + m[0][1] * r.GetY() + m[0][2] * r.GetZ() + m[0][3] * r.GetW(),
+                     m[1][0] * r.GetX() + m[1][1] * r.GetY() + m[1][2] * r.GetZ() + m[1][3] * r.GetW(),
+                     m[2][0] * r.GetX() + m[2][1] * r.GetY() + m[2][2] * r.GetZ() + m[2][3] * r.GetW(),
+                     m[3][0] * r.GetX() + m[3][1] * r.GetY() + m[3][2] * r.GetZ() + m[3][3] * r.GetW());
 
   }
 
   public Mat4f Mul(Mat4f r)
   {
+    Mat4f res = new Mat4f();
 
+    for(int i = 0; i < 4; i++)
+    {
+      for(int j = 0; j < 4; j ++)
+      {
+        res.Set(i, j, m[i][0] * r.Get(0, j) +
+        m[i][1] * r.Get(1, j) +
+        m[i][2] * r.Get(2, j) +
+        m[i][3] * r.Get(3, j));
+      }
+    }
+
+    return res;
   }
 
   public float[][] GetM()
   {
+    float[][] = new float[4][4];
 
+    for(int i = 0; i < 4; i++)
+      for(int j = 0; j < 4; j++)
+        res[i][j] = m[i][j];
+
+    return res;
   }
 
   public float Get(int x, int y)
@@ -97,7 +185,7 @@ public class Mat4f
 
   public void SetM(float[][] m)
   {
-
+    this.m = m;
   }
 
   public void Set(int x, int y)
--- src/Vec4f.java
+++ src/Vec4f.java
@@ -47,57 +47,61 @@ public class Vec4f
 
   public Vec4f Rotate(Vec4f axis, float angle)
   {
+    float sinAngle = (float)Math.sin(-angle);
+    float cosAngle = (float)Math.cos(-angle);
 
+    return this.Cross(axis.Mul(sinAngle).Add((thus.Mul(cosAngle)).Add(
+    axis.Mul(this.Dot(axis.Mul(1 - cosAngle)))));
   }
 
   public Vec4f Lerp(Vec4f dest, float lerpFactor)
   {
-
+    return dest.Sub(this).Mul(lerpFactor).Add(this);
   }
 
   public Vec4f Add(Vec4f r)
   {
-
+    return new Vec4f(x + r.GetX(), y + r.GetY(), z + r.GetZ(), w + r.GetW());
   }
 
   public Vec4f Add(float r)
   {
-
+    return new Vec4f(x + r, y + r, z + r, w + r);
   }
 
   public Vec4f Sub(Vec4f r)
   {
-
+    return new Vec4f(x - r.GetX(), y - r.GetY(), z - r.GetZ(), w - r.GetW());
   }
 
   public Vec4f Sub(float r)
   {
-
+    return new Vec4f(x - r, y - r, z - r, w - r);
   }
 
   public Vec4f Mul(Vec4f r)
   {
-
+    return new Vec4f(x * r.GetX(), y * r.GetY(), z * r.GetZ(), w * r.GetW());
   }
 
   public Vec4f Mul(float r)
   {
-
+    return new Vec4f(x * r, y * r, z * r, w * r);
   }
 
   public Vec4f Div(Vec4f r)
   {
-
+    return new Vec4f(x / r.GetX(), y / r.GetY(), z / r.GetZ(), w / r.GetW());
   }
 
   public Vec4f Div(float r)
   {
-
+    return new Vec4(x / r, y / r, z / r, w / r);
   }
 
   public Vec4f Abs()
   {
-
+    return new Vec4f(Math.abs(x), Math.abs(y), Math.abs(z), Math.abs(w));
   }
 
   public String toString()
