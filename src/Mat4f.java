97c6f2c7cf2173239581c48e836f90dd6e65ead3
Gareth <gareth.w.owen@gmail.com>
2016-02-04 00:40:23 +0000

    More stupid typos...

--- src/Mat4f.java
+++ src/Mat4f.java
@@ -34,7 +34,7 @@ public class Mat4f
   public Mat4f InitTranslation(float x, float y, float z)
   {
     m[0][0] = 1; m[0][1] = 0; m[0][2] = 0; m[0][3] = x;
-    m[1][0] = 0; m[1][1] = 1; m[1][2] = 0; m[0][2] = y;
+    m[1][0] = 0; m[1][1] = 1; m[1][2] = 0; m[1][3] = y;
     m[2][0] = 0; m[2][1] = 0; m[2][2] = 1; m[2][3] = z;
     m[3][0] = 0; m[3][1] = 0; m[3][2] = 0; m[3][3] = 1;
 
@@ -46,7 +46,7 @@ public class Mat4f
     float sin = (float)Math.sin(angle);
     float cos = (float)Math.cos(angle);
 
-    m[0][0] = cos + x * x * (1-cos);     m[0][1] = x *y * (1-cos) - z * sin;  m[0][2] = x * z * (1-cos) + y * sin; m[0][3] = 0;
+    m[0][0] = cos + x * x * (1-cos);     m[0][1] = x * y * (1-cos) - z * sin;  m[0][2] = x * z * (1-cos) + y * sin; m[0][3] = 0;
     m[1][0] = y * x * (1-cos) + z * sin; m[1][1] = cos + y * y * (1-cos);     m[1][2] = y * z * (1-cos) - x * sin; m[1][3] = 0;
     m[2][0] = z * x * (1-cos) - y * sin; m[2][1] = z * y * (1-cos) + x * sin; m[2][2] = cos + z * z * (1-cos);     m[2][3] = 0;
     m[3][0] = 0;                         m[3][1] = 0;                         m[3][2] = 0;                         m[3][3] = 1;
