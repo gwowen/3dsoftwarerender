d13cfc15f96a650befebde7da5691bfd14827000
Gareth <gareth.w.owen@gmail.com>
2016-03-19 01:43:02 +0000

    Started work on indexed models, meshes and obj loading

---
+++ src/OBJModel.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.map;
+
+public class OBJModel
+{
+  
+}
