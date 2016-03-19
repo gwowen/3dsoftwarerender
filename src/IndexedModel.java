dee37867d54f148a5d85e21aff0e4706e58e8cb4
Gareth <gareth.w.owen@gmail.com>
2016-03-19 23:50:24 +0000

    Ah, that habit of not saving before committing...

--- src/IndexedModel.java
+++ src/IndexedModel.java
@@ -3,5 +3,31 @@ import java.util.List;
 
 public class IndexedModel
 {
+  private List<Vec4f> m_positions;
+  private List<Vec4f> m_texCoords;
+  private List<Vec4f> m_normals;
+  private List<Vec4f> m_tangents;
+  private List<Integer> m_indices;
+
+  public IndexedModel()
+  {
+
+  }
+
+  public void CalcNormals()
+  {
+
+  }
+
+  public void CalcTangents()
+  {
+
+  }
+
+  public List<Vec4f> GetPositions() { return m_positions; }
+  public List<Vec4f> GetTexCoords() { return m_texCoords; }
+  public List<Vec4f> GetNormals() { return m_normals; }
+  public List<Vec4f> GetTangents() { return m_tangents; }
+  public List<Integer> GetIndices() { return m_indices; }
 
 }
