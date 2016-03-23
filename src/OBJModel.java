62dd1fec14de4b28ae5f95b07520444a9437c095
Gareth <gareth.w.owen@gmail.com>
2016-03-23 00:25:23 +0000

    Wrote OBJModel loader

--- src/OBJModel.java
+++ src/OBJModel.java
@@ -36,7 +36,16 @@ public class OBJModel
       @Override
       public int hashCode()
       {
+        final int BASE = 17;
+        final int MULTIPLIER = 31;
 
+        int result = BASE;
+
+        result = MULTIPLIER * result + m_vertexIndex;
+        result = MULTIPLIER * result + m_texCoordIndex;
+        result = MULTIPLIER * result + m_normalIndex;
+
+        return result;
       }
   }
 
@@ -49,21 +58,159 @@ public class OBJModel
 
   private static String[] RemoveEmptyStrings(String[] data)
   {
+    List<String> result = new ArrayList<String>();
 
+    for(int i = 0; i < data.length; i++)
+      if(!data[i].equals(""))
+        result.add(data[i]);
+
+    String[] res = new String[result.size()];
+    result.toArray(res);
+
+    return res;
   }
 
   public OBJModel(String filename) throws IOException
   {
+    m_positions = new ArrayList<Vec4f>();
+    m_texCoords = new ArrayList<Vec4f>();
+    m_normal = new ArrayList<Vec4f>();
+    m_indices = new ArrayList<OBJIndex>();
+    m_hasTexCoords = false;
 
+    BufferedReader meshReader = null;
+
+    meshReader = new BufferedReader(new FileReader(fileName))
+    String line;
+
+    while((line = meshReader.readLine()) != null)
+    {
+      String[] tokens = line.split(" ");
+      tokens = RemoveEmptyStrings(tokens);
+
+      if(tokens.length == 0 || tokens[0].equals("#"))
+        continue;
+      else if(tokens[0].equals("v"))
+      {
+        m_positions.add(new Vec4f(Float.valueOf(tokens[1]),
+                        Float.valueOf(tokens[2]),
+                        Float.valueOf(tokens[3]), 1));
+      }
+      else if(tokens[0].equals("vt"))
+      {
+        m_texCoords.add(new Vec4f(Float.valuesOf(tokens[1]),
+                        1.0 - Float.valueOf(tokens[2]), 0, 0));
+      }
+      else if(tokens[0].equals("vn"))
+      {
+        m_normals.add(new Vec4f(Float.valueOf(tokens[1]),
+                      Float.valueOf(tokens[2]),
+                      Float.valueOf(tokens[3]), 0));
+      }
+      else if(tokens[0].equals("f"))
+      {
+        for(int i = 0; i < tokens.length - 3; i++)
+        {
+          m_indices.add(ParseOBJIndex(tokens[1]));
+          m_indices.add(ParseOBJIndex(tokens[2 + i]));
+          m_indices.add(ParseOBJIndex(tokens[3 + i]));
+        }
+      }
+    }
+
+    meshReader.close();
   }
 
   public IndexedModel ToIndexedModel()
   {
+    IndexedModel result = new IndexedModel();
+    IndexedModel normalModel = new IndexedModel();
+    Map<OBJIndex, Integer> resultIndexMap = new HashMap<OBJIndex, Integer>();
+    Map<Integer, Integer> normalIndexMap = new HashMap<Integer, Integer>();
+    Map<Integer, Integer> indexMap = new HashMap<Integer, Integer>();
 
+    for(int i = 0; i < m_indices.size(); i++)
+    {
+      OBJIndex currentIndex = m_indices.get(i);
+
+      Vec4f currentPosition = m_positions.get(currentIndex.GetVertexIndex());
+      Vec4f currentTexCoord;
+      Vec4f currentNormal;
+
+      if(m_hasTexCoords)
+        currentTexCoord = m_texCoords.get(currentIndex.GetTexCoordIndex());
+      else
+        currentNormal = new Vec4f(0,0,0,0);
+
+      Integer modelVertexIndex = resultIndexMap.get(currentIndex);
+
+      if(modelVertexIndex == null)
+      {
+        modelVertexIndex = result.GetPositions().size();
+        resultIndexMap.put(currentIndex, modelVertexIndex);
+
+        result.GetPositions().add(currentPosition);
+        result.GetTexCoords().add(currentTexCoord);
+        if(m_hasNormals)
+          result.GetNormals().add(currentNormal);
+      }
+
+      Integer normalModelIndex = normalIndexMap.get(currentIndex.GetVertexIndex());
+
+      if(normalModelIndex == null)
+      {
+        normalModelIndex = normalModel.GetPositions().size();
+        normalIndexMap.put(currentIndex.GetVertexIndex(), normalModelIndex);
+
+        normalModel.GetPositions().add(currentPosition);
+        normalModel.GetTexCoords().add(currentTexCoord);
+        normalModel.GetNormals().add(currentNormal);
+        normalModel.GetTangents().add(new Vec4f(0,0,0,0));
+      }
+
+      result.GetIndices().add(modelVertexIndex);
+      normalModel.GetIndices().add(normalModelIndex);
+      indexMap.put(modelVertexIndex, normalModelIndex);
+    }
+
+    if(!m_hasNormals)
+    {
+      normalModel.CalcNormals();
+
+      for(int i = 0; i < result.GetPositions().size(); i++)
+        result.GetNormals().add(normalModel.GetNormals().get(indexMap.get(i)));
+    }
+
+    normalModel.CalcTangents();
+
+    for(int i = 0; i < result.GetPositions().size(); i++)
+      result.GetTangents().add(normalModel.GetTangents().get(indexMap.get(i)));
+
+    return result;
   }
 
   private OBJIndex ParseOBJIndex(String token)
   {
+    String[] values = token.split("/");
 
+    OBJIndex result = new OBJIndex();
+    result.SetVertexIndex(Integer.parseInt(values[0]) - 1);
+
+    if(values.length > 1)
+    {
+      if(!values[1].isEmpty())
+      {
+        m_hasTexCoords = true;
+        result.SetTexCoordIndex(Integer.parseInt(values[1]) - 1);
+      }
+
+      if(values.length > 2)
+      {
+        m_hasNormals = true;
+        result.SetNormalIndex(Integer.parseInt(values[2]) - 1);
+      }
+    }
+
+    return result;
   }
 }
